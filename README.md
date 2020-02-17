eb258ce44315e8fad286b1304a082aec63145360
Daniel Lima <daniellima@medlynx.com.br>
2020-02-17 08:29:58 -0300

    criado instalador e adicionado jar do iris

--- README.md
+++ README.md
@@ -7,15 +7,23 @@
     * pyswip
 
 ### Instalando em distribuições baseadas em Debian:
+    Para instalar basta executar o arquivo install.sh como sudo ou executar os seguintes comandos:
+
     # Instalando o SwiProlog
     sudo apt-get install software-properties-common
     sudo apt-add-repository ppa:swi-prolog/stable
     sudo apt-get update
     sudo apt-get install swi-prolog
 
-    #Instalando o Python 3.* e o PIP
+    # Instalando o Python 3.* e o PIP
     sudo apt-get install python3.6
     sudo apt-get install python3-pip
 
     #Instalando a biblioteca pyswip
     pip3 install pyswip
+
+    # Para executar o Íris benchmark
+    sudo apt-get install default-jre
+
+    # Para executar o wsml engine e íris como serviço
+    pip3 install flask
---
+++ install.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+## Instalador testado em sistemas baseados em debian
+
+if [ "$EUID" -ne 0 ]; then
+    echo "Favor executar o script como sudo";
+    exit;
+fi
+
+install () {
+	if [ -z "$(command -v $1)" ]; then
+        if [ -x "$(command -v apt-get)" ]; then
+            sudo apt-get -y install $2
+        elif [ -x "$(command -v yum)" ]; then
+            sudo yum install $3 -y
+        elif [ -x "$(command -v dnf)" ]; then
+            sudo dnf install $3 -y
+        else
+            echo "$2 não encontrado, favor instalar";
+            exit;
+        fi
+    else
+    	echo "$2 já instalado"
+    fi
+}
+
+echo 'Instalando...'
+
+install java default-jre default-jre
+install python3 python3 python3
+install pip3 pip3 pip3
+install swipl swi-prolog pl
+pip3 install pyswip
+pip3 install flask
+
+echo 'Finalizado...'

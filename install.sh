1c0f2682953f10902c703edcbcc932427737ea86
Daniel Lima Oliveira <daniel102510@hotmail.com>
2020-10-29 06:47:44 -0300

    fix: Fix problem with pip3 and default-jre instalation on ubuntu

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ if [ "$EUID" -ne 0 ]; then
 fi
 
 install () {
-	if [ -z "$(command -v $1)" ]; then
+	#if [ -z "$(command -v $1)" ]; then
         if [ -x "$(command -v apt-get)" ]; then
             sudo apt-get -y install $2
         elif [ -x "$(command -v yum)" ]; then
@@ -19,16 +19,16 @@ install () {
             echo "$2 não encontrado, favor instalar";
             exit;
         fi
-    else
-    	echo "$2 já instalado"
-    fi
+    #else
+    #	echo "$2 já instalado"
+    #fi
 }
 
 echo 'Instalando...'
 
 install java default-jre default-jre
 install python3 python3 python3
-install pip3 pip3 pip3
+install pip3 python3-pip pip3
 install swipl swi-prolog pl
 pip3 install pyswip
 pip3 install flask

4e2c91ec755c5a350650a99eb04ce8f3b959f1b1
Daniel Lima Oliveira <daniel102510@hotmail.com>
2020-12-22 20:50:43 -0300

    feat: Ping tests

--- service/midsi-client.py
+++ service/midsi-client.py
@@ -21,9 +21,18 @@ if len(sys.argv) > 1 and sys.argv[1] in ['--clear', '--ontology', '--query', '--
     if message != '':
         try:
             sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            
+            # Ping with the server
+            sock.sendto('ping'.encode('utf-8'), (UDP_IP, UDP_PORT))
+            sock.settimeout(2)
+            data, server = sock.recvfrom(4096)
+
             sock.sendto(message.encode('utf-8'), (UDP_IP, UDP_PORT))
+            sock.settimeout(None)
             data, server = sock.recvfrom(4096)
             print(data.decode('utf-8'))
+        except Exception as ex:
+            print('\n* midsi-service.py is running?')
         finally:
             pass
 else:
--- service/midsi-service.py
+++ service/midsi-service.py
@@ -9,44 +9,55 @@ reasoner = Reasoner()
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((UDP_IP, UDP_PORT))
 
+print('Server ' + str(UDP_IP) + ' start on port ' + str(UDP_PORT))
+
 while True:
     data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-    args = data.decode('utf-8').split('=')
-    command = args[0]
-    if command == 'clear':
-        start = time.time()
-        reasoner.clear()
-        finish = time.time()
-        countTime = (finish-start)*1000
-        response = 'Cleared in ' + str(round(countTime, 2)) + ' ms'
-        sock.sendto(response.encode('utf_8'), addr)
-    elif command == 'ontology':
-        start = time.time()
-        reasoner.load(args[1])
-        finish = time.time()
-        countTime = (finish-start)*1000
-        response = 'Loaded in ' + str(round(countTime, 2)) + ' ms'
-        sock.sendto(response.encode('utf_8'), addr)
-    elif command == 'query':
-        start = time.time()
-        result = reasoner.execute(args[1])
-        finish = time.time()
-        countTime = (finish-start)*1000
-        response = str(result)+'\nQuery finished in ' + str(round(countTime, 2)) + ' ms'
-        sent = sock.sendto(response.encode('utf_8'), addr)
-    elif command == 'queryFile':
-        if (os.path.exists(args[1])):
-            f = open(args[1] ,'r')
-            query = f.read()
-            f.close()
+    dataRecieve = data.decode('utf-8')
+    if dataRecieve == 'ping':
+        sock.sendto(data, addr)
+    else:
+        args = dataRecieve.split('=')
+        command = args[0]
+        if command == 'clear':
+            print('Command: clear')
             start = time.time()
-            result = reasoner.execute(query)
+            reasoner.clear()
+            finish = time.time()
+            countTime = (finish-start)*1000
+            response = 'Cleared in ' + str(round(countTime, 2)) + ' ms'
+            sock.sendto(response.encode('utf_8'), addr)
+        elif command == 'ontology':
+            print('Command: load ontology ' + args[1])
+            start = time.time()
+            reasoner.load(args[1])
+            finish = time.time()
+            countTime = (finish-start)*1000
+            response = 'Loaded in ' + str(round(countTime, 2)) + ' ms'
+            sock.sendto(response.encode('utf_8'), addr)
+        elif command == 'query':
+            print('Command: execute query ' + args[1])
+            start = time.time()
+            result = reasoner.execute(args[1])
             finish = time.time()
             countTime = (finish-start)*1000
             response = str(result)+'\nQuery finished in ' + str(round(countTime, 2)) + ' ms'
             sent = sock.sendto(response.encode('utf_8'), addr)
+        elif command == 'queryFile':
+            print('Command: execute query file ' + args[1])
+            if (os.path.exists(args[1])):
+                f = open(args[1] ,'r')
+                query = f.read()
+                f.close()
+                start = time.time()
+                result = reasoner.execute(query)
+                finish = time.time()
+                countTime = (finish-start)*1000
+                response = str(result)+'\nQuery finished in ' + str(round(countTime, 2)) + ' ms'
+                sent = sock.sendto(response.encode('utf_8'), addr)
+            else:
+                print('File not found')
+                response = 'File not found'
+                sent = sock.sendto(response.encode('utf_8'), addr)
         else:
-            response = 'File not found'
-            sent = sock.sendto(response.encode('utf_8'), addr)
-    else:
-        sent = sock.sendto(b'Command invalid!', addr)
+            sent = sock.sendto(b'Command invalid!', addr)
